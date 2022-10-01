a04dcdf3f6492ce62cda6d6873432965d0d6459f
Grayzen <guraymanav@gmail.com>
2022-10-02 01:31:15 +0300

    first commit

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+
+        <div class="flex h-screen">
+            <slot />
+        </div>
+
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+})
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</Link>
+
+            <template v-else>
+                <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</Link>
+
+                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</Link>
+            </template>
+        </div>
+
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 dark:text-gray-200 text-4xl font-mono">
+            Todo App
+        </div>
+    </div>
+</template>
